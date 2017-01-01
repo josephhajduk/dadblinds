e66ad61344a9b8c4fddc1f312d734a216b4d0077
mhajduk <michael.hajduk@gmail.com>
2016-12-31 18:17:25 -0700

    modified russound stuff

--- somfy.py
+++ somfy.py
@@ -7,10 +7,10 @@ from telnetlib import Telnet
 
 @asyncio.coroutine
 def rus(request):
-    command = request.match_info.get('command', "VERSION")
-    print("RUS COMMAND:"+command)
+    command = request.match_info.get('command', "VERSION").replace("_"," ").replace("!","\"").encode("ascii","ignore")
+    print("RUS COMMAND:"+str(command))
     tn = Telnet('192.168.2.227', 9621)
-    tn.write(command.encode("ascii")+b"\r")
+    tn.write(command+b"\r")
     response = tn.read_until(b"\r", timeout=10)
     tn.close()
     response_str = response.decode("ascii")
