d12b0596b69ca1edef07826de2e326a016f84c82
mhajduk <michael.hajduk@gmail.com>
2016-12-31 17:33:16 -0700

    renamed and removed stuff

--- somfy.py
+++ somfy.py
@@ -6,7 +6,7 @@ import asyncio
 @asyncio.coroutine
 def down(request):
     motor = request.match_info.get('motor', "01")
-    print(motor)
+    print("MOTOR:"+motor+" COMMAND: DOWN")
     ser = serial.Serial(
         port='/dev/ttyUSB0',
         baudrate=9600,
@@ -22,7 +22,7 @@ def down(request):
 @asyncio.coroutine
 def up(request):
     motor = request.match_info.get('motor', "01")
-    print(motor)
+    print("MOTOR:"+motor+" COMMAND: UP")
     ser = serial.Serial(
         port='/dev/ttyUSB0',
         baudrate=9600,
@@ -38,6 +38,7 @@ def up(request):
 @asyncio.coroutine
 def stop(request):
     motor = request.match_info.get('motor', "01")
+    print("MOTOR:"+motor+" COMMAND: STOP")
     print(motor)
     ser = serial.Serial(
         port='/dev/ttyUSB0',
@@ -51,28 +52,9 @@ def stop(request):
     ser.close()
     return web.Response(text="up")
 
-@asyncio.coroutine
-def myposition(request):
-    motor = request.match_info.get('motor', "01")
-    print(motor)
-    ser = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-    )
-    ser.write(("01"+motor+"S\r").encode())
-    time.sleep(.1)
-    ser.write(("01"+motor+"S\r").encode())
-    ser.close()
-    return web.Response(text="myposition")
-
 app = web.Application()
 app.router.add_route("POST", '/{motor}/up', up)
 app.router.add_route("POST", '/{motor}/down', down)
-app.router.add_route("POST", '/{motor}/myposition', myposition)
 app.router.add_route("POST", '/{motor}/stop', stop)
 
 web.run_app(app)
